aac86a90f452644f997ddde44839d58714188f32
Usuario <Usuario@CPD02-M05>
2024-03-04 12:20:36 -0300

    Avanco no projeto

--- MauiListaCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiListaCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MauiListaCompras.Models;
+﻿using MauiListaCompras.Models;
 using SQLite;
 
 namespace MauiListaCompras.Helpers
@@ -11,14 +6,44 @@ namespace MauiListaCompras.Helpers
     public class SQLiteDatabaseHelper
     {
         readonly SQLiteAsyncConnection _conn;
+
         public SQLiteDatabaseHelper(string path)
         {
             _conn = new SQLiteAsyncConnection(path);
             _conn.CreateTableAsync<Produto>().Wait();
         }
+
+
         public Task<int> Insert(Produto p)
         {
             return _conn.InsertAsync(p);
+        }
+
+
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE produto SET descricao=?, " + "Quantidade=?, Preco=? WHERE Id=?";
+
+            return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
+        }
+
+        public Task<List<Produto>> GetAll() 
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<int> Delete(int id) 
+        {
+            return _conn.Table<Produto>().DeleteAsync(
+                i => i.Id == id);
+        }
+
+        public Task<List<Produto>> Search(string q) 
+        {
+            string sql = "SELECT * FROM Produto WHERE " +
+                "descricao LIKE '%" + q + "%'";
+
+            return _conn.QueryAsync<Produto>(sql);
         }
     }
 }
